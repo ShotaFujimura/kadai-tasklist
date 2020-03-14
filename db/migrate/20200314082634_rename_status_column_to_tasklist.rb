11ef203a50026bb37467b4d18a5245c050bdeefd
Sho.tanaka <sho.tiku1108@gmail.com>
2020-03-14 08:33:30 +0000

    modify Status to status" ;

---
+++ db/migrate/20200314082634_rename_status_column_to_tasklist.rb
@@ -0,0 +1,5 @@
+class RenameStatusColumnToTasklist < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :tasks, :Status, :status
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_011407) do
+ActiveRecord::Schema.define(version: 2020_03_14_082634) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "Status"
+    t.string "status"
   end
 
 end
