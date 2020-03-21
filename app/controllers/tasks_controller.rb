a30894f8ce498d6f5363f2933fad73c13631a74e
Sho.tanaka <sho.tiku1108@gmail.com>
2020-03-21 16:38:57 +0000

    modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
-    before_action :require_user_logged_in, only: [:index,:create, :destroy]
+    before_action :set_task, only: [:edit, :show, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:edit, :show, :update, :destroy]
     
     def index
       if logged_in?
@@ -26,7 +27,7 @@ class TasksController < ApplicationController
         else
             @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'Task was not posted'
-            render 'tasks/index'
+            render 'tasks/new'
         end 
     end 
     
@@ -60,5 +61,12 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content, :status)
     end 
+    
+    def correct_user
+      @task = current_user.microposts.find_by(id: params[:id])
+    　 unless @task
+   　   　redirect_to root_url
+     　end
+    end
     
 end
